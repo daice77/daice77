85ec6ce9281b238c690b2f238c83487bdd31c061
Olaf Kupschina <olaf.kupschina@gmail.com>
2025-11-27 14:23:21 +0100

    extra chatkit files

---
+++ chatkit/bundle/index-Cva98ZVp.js
@@ -0,0 +1,2 @@
+const __vite__mapDeps=(i,m=__vite__mapDeps,d=(m.f||(m.f=["assets/ck1/index.umd-ZMdCe26M.js","assets/ck1/index-B5Rsjn5T.js","assets/ck1/index-DfnonTzj.css"])))=>i.map(i=>d[i]);
+import{w as n,x as o,y as a}from"./index-B5Rsjn5T.js";function u(i){return n(this,void 0,void 0,function(){var t;return o(this,function(e){switch(e.label){case 0:return[4,a(()=>import("./index.umd-ZMdCe26M.js").then(r=>r.i),__vite__mapDeps([0,1,2]))];case 1:return t=e.sent(),i._plugins=t,[2]}})})}export{u as loadLegacyVideoPlugins};
